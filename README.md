4cd1bac597aeed56e4ea8ad920dc46a218a7de9f
Demari Miller <demarijmiller@gmail.com>
2025-05-20 13:19:32 -0400

    Setup basic TypeScript action

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ai-test-suggester
+
+This repository contains a simple GitHub Action implemented in TypeScript.
---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.run = run;
+function run() {
+    console.log('Hello from ai-test-suggester action');
+}
+run();
